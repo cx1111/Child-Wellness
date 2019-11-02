bf011404702e028728215c5316ae7bc7dab8a9b5
Reed Jessen <jessen.reed@bcg.com>
2019-11-02 13:15:39 -0400

    initial commit

---
+++ src/audio_cleaner.py
@@ -0,0 +1,25 @@
+
+
+
+class AudioCleaner:
+
+    def __init__(self):
+        pass
+
+    def clean_audio(self,audio_file):
+        self.shift_pitch_down()
+
+    def shift_pitch_down(self):
+        pass
+
+    def identify_child_voice(self):
+        pass
+
+    def denoise_background(self):
+        pass
+
+    def adjust_volume(self):
+        pass
+
+    def adjust_sample_rate(self):
+        pass
