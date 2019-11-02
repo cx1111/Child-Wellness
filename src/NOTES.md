0111efa89859c23e46491e20210f4c5e9521ce9b
Chen Xie <Xie.Chen@bcg.com>
2019-11-02 15:55:16 -0400

    Add html templates (#26)
    * Add base html file
    
    * Add d3 to home
    
    * Add video detail template

---
+++ src/NOTES.md
@@ -0,0 +1,18 @@
+## Ideas
+
+Content stored in gphotos. No liability.
+
+Action items/solutions. ie. Consult your doctor? Tell kid to tell you a story (NO LIABILITY).
+
+Add interactive slideshows. Show animals/objects
+
+Label (show) the 3 different lines. Little Johnny.
+
+Predicted growth (faded)
+
+Split audio, STFT, filter by pitch.
+
+
+Videos are not indicative of their full vocab
+
+Company Name?
--- src/app/childwords.py
+++ src/app/childwords.py
@@ -65,7 +65,11 @@ def video_detail(video_name):
     """
     Show the video and associated detail
     """
-    return 'Video {}'.format(video_name)
+    # Hardcode url for now
+    video_info = {
+        'title': video_name,
+        'url': 'https://drive.google.com/file/d/11dRucTxdfx6Soy_ldkzSaiDJ1I2eSV72/preview', }
+    return render_template('video_detail.html', video_info=video_info)
 
 
 @app.route('/realhome')
