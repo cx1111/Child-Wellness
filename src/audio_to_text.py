c42e0b722022d480f91f08b89f2e86ef318b6988
Reed Jessen <jessen.reed@bcg.com>
2019-11-02 09:41:13 -0400

    pep8 reformat

--- src/audio_to_text.py
+++ src/audio_to_text.py
@@ -2,17 +2,14 @@ import boto3
 import os
 from datetime import datetime
 
-def transcribe_audio(audio_uri,output_bucket):
 
-
+def transcribe_audio(audio_uri, output_bucket):
     client = boto3.client('transcribe')
-
 
     file_name = os.path.basename(audio_uri).split('.')[0] + datetime.now().strftime('%Y%m%d%H%m')
     file_format = os.path.basename(audio_uri).split('.')[1]
 
     output_path = output_bucket + '/' + file_name + '.json'
-
 
     response = client.start_transcription_job(
         TranscriptionJobName=file_name,
@@ -24,7 +21,6 @@ def transcribe_audio(audio_uri,output_bucket):
         OutputBucketName=output_bucket,
 
         Settings={
-            # 'VocabularyName': 'string',
             'ShowSpeakerLabels': True,
             'MaxSpeakerLabels': 10,
             'ChannelIdentification': False
@@ -32,9 +28,3 @@ def transcribe_audio(audio_uri,output_bucket):
     )
 
     return output_path, response
-
-
-input_file = 'https://dvhacksaudioinput.s3-us-west-1.amazonaws.com/kids1.mp3'
-output_bucket = 'dvhacksout'
-result = transcribe_audio(input_file,output_bucket)
-print(result)
