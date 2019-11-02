cf5ac6de81d8a34c717ee5e8e9acde3e29f693e6
Reed Jessen <jessen.reed@bcg.com>
2019-11-02 11:00:10 -0400

    adding doc strings

--- src/audio_to_text.py
+++ src/audio_to_text.py
@@ -5,6 +5,15 @@ import config
 
 
 def transcribe_audio(audio_uri, output_bucket=config.BUCKET_TRANSCRIPTION):
+    """
+    Convert the given audio file to an transcription file in the specified s3 bucket
+
+    Parameters:
+        audio_uri (str): Source audio file s3 uri to be converted
+        output_bucket (str): Destination for the transcription json file
+    """
+
+
     client = boto3.client('transcribe')
 
     file_name = os.path.basename(audio_uri).split('.')[0] + datetime.now().strftime('%Y%m%d%H%m%s')
@@ -33,7 +42,13 @@ def transcribe_audio(audio_uri, output_bucket=config.BUCKET_TRANSCRIPTION):
 
 
 def upload_audio(file_path,bucket=config.BUCKET_AUDIO):
+    """
+    Uploads a file to the target s3 bucket
 
+    Parameters:
+        file_path (str): Source file to be uploaded
+        bucket (str): Destination for the uploaded file
+    """
     file_name = os.path.basename(file_path)
 
     s3 = boto3.client('s3')
@@ -45,6 +60,13 @@ def upload_audio(file_path,bucket=config.BUCKET_AUDIO):
 
 
 def download_transcription(input_path,output_folder):
+    """
+    Uploads downloads a file from s3 and saves to target location
+
+    Parameters:
+        input_path (str): s3 file to be downloaded in <bucket_name>/<file_name> format
+        output_folder (str): target folder where file will be saved
+    """
     file_name = input_path.split('/')[1]
     bucket = input_path.split('/')[0]
     download_target = os.path.join(output_folder,file_name)
