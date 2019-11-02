6dd550cc47cbc048b54bfd3a8b3eb1ea58a555da
Reed Jessen <jessen.reed@bcg.com>
2019-11-02 09:39:34 -0400

    parameterize

--- src/audio_to_text.py
+++ src/audio_to_text.py
@@ -1,32 +1,40 @@
 import boto3
 import os
-
+from datetime import datetime
 
 def transcribe_audio(audio_uri,output_bucket):
 
 
     client = boto3.client('transcribe')
 
-    file_name = os.path.basename()
+
+    file_name = os.path.basename(audio_uri).split('.')[0] + datetime.now().strftime('%Y%m%d%H%m')
+    file_format = os.path.basename(audio_uri).split('.')[1]
+
+    output_path = output_bucket + '/' + file_name + '.json'
 
 
     response = client.start_transcription_job(
         TranscriptionJobName=file_name,
-        LanguageCode='en-US'|'es-US'|'en-AU'|'fr-CA'|'en-GB'|'de-DE'|'pt-BR'|'fr-FR'|'it-IT'|'ko-KR'|'es-ES'|'en-IN'|'hi-IN'|'ar-SA'|'ru-RU'|'zh-CN',
-        MediaSampleRateHertz=123,
-        MediaFormat='mp3'|'mp4'|'wav'|'flac',
+        LanguageCode='en-US',
+        MediaFormat=file_format,
         Media={
             'MediaFileUri': audio_uri
         },
         OutputBucketName=output_bucket,
-        OutputEncryptionKMSKeyId='string',
+
         Settings={
-            'VocabularyName': 'string',
+            # 'VocabularyName': 'string',
             'ShowSpeakerLabels': True,
             'MaxSpeakerLabels': 10,
             'ChannelIdentification': False
         }
     )
 
-    return response
+    return output_path, response
 
+
+input_file = 'https://dvhacksaudioinput.s3-us-west-1.amazonaws.com/kids1.mp3'
+output_bucket = 'dvhacksout'
+result = transcribe_audio(input_file,output_bucket)
+print(result)
