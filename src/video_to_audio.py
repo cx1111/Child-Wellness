309224eeacb7ef50766dc18af4e97cbe00f12fc3
Mendoza-Andrew <Mendoza.Andrew@bcg.com>
2019-11-01 20:55:48 -0400

    Convert multiple different video types (#9)
    * Add requirements
    
    * Add support for multiple file extensions

--- src/video_to_audio.py
+++ src/video_to_audio.py
@@ -8,8 +8,8 @@ parser.add_argument('video_dir', type=str,
 					help='directory containing videos to convert to audio')
 parser.add_argument('dest_dir', type=str,
 					help='directory to store converted audio files')
-parser.add_argument('--video_ext', type=str, default='.mp4',
-					help='extension of video files to convert')
+parser.add_argument('--video_ext', type=str, nargs='+', default=['.mp4'],
+					help='extensions of video files to convert')
 parser.add_argument('--dest_ext', type=str, default='.mp3',
 					help='extension audio files will be converted to')
 args = parser.parse_args()
@@ -26,7 +26,7 @@ def convert_video(video_file, dest_audio_file):
 	video = mp.VideoFileClip(video_file)
 	video.audio.write_audiofile(dest_audio_file)
 
-def get_dest_audio_file(video_file, video_ext, dest_dir, dest_ext):
+def get_dest_audio_file(video_file, dest_dir, dest_ext):
 	"""
 	Create the destination path for the converted audio file
 
@@ -37,6 +37,7 @@ def get_dest_audio_file(video_file, video_ext, dest_dir, dest_ext):
 		dest_ext (str): Extension that the audio file will be converted to
 	"""
 	video_basename = os.path.basename(video_file)
+	_, video_ext = os.path.splitext(video_basename)
 	audio_basename = video_basename.replace(video_ext, dest_ext)
 	return os.path.join(dest_dir, audio_basename)
 
@@ -50,7 +51,7 @@ def create_dir_if_not_exists(dir_path):
 	if not os.path.exists(dir_path):
 		os.makedirs(dir_path)
 
-def convert_videos_in_dir(video_dir, dest_dir, video_ext='.mp4', dest_ext='.mp3'):
+def convert_videos_in_dir(video_dir, dest_dir, video_ext=['.mp4'], dest_ext='.mp3'):
 	"""
 	Convert all videos in the given directory of the specified type to audio files
 	that will be stored in the provided directory
@@ -65,11 +66,14 @@ def convert_videos_in_dir(video_dir, dest_dir, video_ext='.mp4', dest_ext='.mp3'
 	# Ensure that dest dir exists
 	create_dir_if_not_exists(dest_dir)
 
+	# Gather all video files to convert
+	video_files = []
+	for ext in video_ext:
+		video_files += glob.glob(os.path.join(video_dir, "*{}".format(ext)))
+
 	# Convert video files in the dir to audio files in the dest dir
-	video_files = glob.glob(os.path.join(video_dir, "*{}".format(video_ext)))
 	for video_file in video_files:
-		dest_audio_file = get_dest_audio_file(video_file, video_ext,
-											  dest_dir, dest_ext)
+		dest_audio_file = get_dest_audio_file(video_file, dest_dir, dest_ext)
 		convert_video(video_file, dest_audio_file)
 
 if __name__ == '__main__':
